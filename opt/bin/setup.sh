45112d9a7ab30b40fd9705ab5a0462476c9212ec
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2016-02-16 12:11:04 +0200

    [feature] Added some required R libraries

--- opt/bin/setup.sh
+++ opt/bin/setup.sh
@@ -2,7 +2,7 @@
 cat /opt/telostat/Rprofile.R > /etc/R/Rprofile.site
 
 ## Install required packages:
-/opt/telostat/bin/install.sh rmarkdown vst/sepl
+/opt/telostat/bin/install.sh vst/sepl rmarkdown dplyr lubridate ggplot2 zoo xts data.table httr
 
 ## Copy the web stuff:
 cp -R /opt/telostat/www/* /var/www/html/
